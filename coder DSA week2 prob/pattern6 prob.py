ad8aed30b4787884f0a5a1911f6a84e4122a8c00
Pavi-sk33 <skp200355@gmail.com>
2025-02-27 20:33:49 +0530

    Add files via upload

---
+++ coder DSA week2 prob/pattern6 prob.py
@@ -0,0 +1,5 @@
+n=int(input('no:'))
+for i in range(n):
+    for j in range(1,n-i+1):
+        print('*',end='')
+    print('')
---
+++ coder DSA week2 prob/pattern9 prob.py
@@ -0,0 +1,7 @@
+n=int(input('no:'))
+count=1
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(count,end=' ')
+    print('')
+    count=count+1
